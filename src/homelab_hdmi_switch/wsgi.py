b8932c096bce85235eb16fc87d96ca36ac927832
Matthew J. Morrison <mattjmorrison@mattjmorrison.com>
2026-08-14 03:07:47 +0000

    update app

--- src/homelab_hdmi_switch/wsgi.py
+++ src/homelab_hdmi_switch/wsgi.py
@@ -3,7 +3,7 @@ from wsgiref.types import StartResponse, WSGIEnvironment
 
 
 def app(environ: WSGIEnvironment, start_response: StartResponse) -> list[bytes]:
-    body = json.dumps({"Hello": "World"}).encode("utf-8")
+    body = json.dumps({"Hello - 2": "World - 2"}).encode("utf-8")
     headers = [
         ("Content-Type", "application/json"),
         ("Content-Length", str(len(body))),
